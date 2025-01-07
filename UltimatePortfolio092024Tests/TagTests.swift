307c8cbc67064db04fd44d06e3f86ded514ce161
KeyBarreto <keylab@gmail.com>
2025-01-07 17:27:57 -0400

    Video 24. Testing Core Data

---
+++ UltimatePortfolio092024Tests/TagTests.swift
@@ -0,0 +1,45 @@
+//
+//  TagTests.swift
+//  UltimatePortfolio092024Tests
+//
+//  Created by Keyla Barreto on 01/07/25.
+//
+
+import CoreData
+import XCTest
+@testable import UltimatePortfolio092024
+
+final class TagTests: BaseTestCase {
+    func testCreatingTagsAndIssues() {
+        let targetCount = 10
+
+        for _ in 0..<targetCount {
+            let tag = Tag(context: mananedObjectContext)
+
+            for _ in 0..<targetCount {
+                let issue = Issue(context: mananedObjectContext)
+                tag.addToIssues(issue)
+            }
+        }
+
+        XCTAssertEqual(dataController.count(for: Tag.fetchRequest()),
+                       targetCount,
+                       "There should be \(targetCount) tags.")
+        XCTAssertEqual(dataController.count(for: Issue.fetchRequest()),
+                       targetCount * targetCount,
+                       "There should be \(targetCount * targetCount) issues.")
+    }
+
+    func testDeletingTagDoesNotDeleteIssues() throws {
+        dataController.createSampleData()
+
+        let request = NSFetchRequest<Tag>(entityName: "Tag")
+        let tags = try mananedObjectContext.fetch(request)
+        dataController.delete(tags[0])
+
+        XCTAssertEqual(dataController.count(for: Tag.fetchRequest()), 4,
+                       "Expected 4 tags after deleting 1.")
+        XCTAssertEqual(dataController.count(for: Issue.fetchRequest()), 50,
+                       "Expected 50 issues after deleting a tag.")
+    }
+}
