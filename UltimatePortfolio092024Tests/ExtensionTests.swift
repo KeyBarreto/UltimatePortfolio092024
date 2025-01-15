567ab20520ea67d0215144088f5bb5655f7223ea
KeyBarreto <keylab@gmail.com>
2025-01-14 21:39:57 -0400

    Video 26. Testing extensions

---
+++ UltimatePortfolio092024Tests/ExtensionTests.swift
@@ -0,0 +1,150 @@
+//
+//  ExtensionTests.swift
+//  UltimatePortfolio092024Tests
+//
+//  Created by Keyla Barreto on 01/14/25.
+//
+
+import CoreData
+import XCTest
+@testable import UltimatePortfolio092024
+
+final class ExtensionTests: BaseTestCase {
+    func testIssueTitleUnwrap() {
+        // Given
+        let issue = Issue(context: managedObjectContext)
+
+        // When
+        issue.title = "Example issue"
+        // Then
+        XCTAssertEqual(issue.issueTitle, "Example issue", "Changing title should also change issueTitle")
+
+        // When
+        issue.issueTitle = "Updated issue"
+        // Then
+        XCTAssertEqual(issue.title, "Updated issue", "Changing issueTitle should also change title")
+    }
+
+    func testIssueContentUnwrap() {
+        let issue = Issue(context: managedObjectContext)
+
+        issue.content = "Example issue"
+        XCTAssertEqual(issue.issueContent, "Example issue", "Changing content should also change issueContent")
+
+        issue.issueContent = "Updated issue"
+        XCTAssertEqual(issue.content, "Updated issue", "Changing issueContent should also change content")
+    }
+
+    func testIssueCreationDateUnwrap() {
+        let issue = Issue(context: managedObjectContext)
+        let testDate = Date.now
+
+        issue.creationDate = testDate
+        XCTAssertEqual(issue.issueCreationDate, testDate, "Changing creationDate should also change issueCreationDate")
+    }
+
+    func testIssueTagsUnwrap() {
+        let tag = Tag(context: managedObjectContext)
+        let issue = Issue(context: managedObjectContext)
+
+        XCTAssertEqual(issue.issueTags.count, 0, "A new issue should have no tags.")
+
+        issue.addToTags(tag)
+        XCTAssertEqual(issue.issueTags.count, 1, "Adding 1 tag to an issue should result in issueTags having count 1.")
+    }
+
+    func testIssueTagsList() {
+        let tag = Tag(context: managedObjectContext)
+        let issue = Issue(context: managedObjectContext)
+
+        tag.name = "My tag"
+        issue.addToTags(tag)
+
+        XCTAssertEqual(issue.issueTagsList, "My tag", "Adding 1 tag to an issue should make issueTagsList be My Tag.")
+    }
+
+    func testIssueSortingIsStable() {
+        let issue1 = Issue(context: managedObjectContext)
+        issue1.title = "B Issue"
+        issue1.creationDate = .now
+
+        let issue2 = Issue(context: managedObjectContext)
+        issue2.title = "B Issue"
+        issue2.creationDate = .now.addingTimeInterval(1)
+
+        let issue3 = Issue(context: managedObjectContext)
+        issue3.title = "A Issue"
+        issue3.creationDate = .now.addingTimeInterval(100)
+
+        let allIssues = [issue1, issue2, issue3]
+        let sorted = allIssues.sorted()
+
+        XCTAssertEqual([issue3, issue1, issue2], sorted, "Sorting issue arrays should use title then creationDate")
+    }
+
+    func testTagIDUnwrap() {
+        let tag = Tag(context: managedObjectContext)
+
+        tag.id = UUID()
+        XCTAssertEqual(tag.tagID, tag.id, "Changing id should also change tagID")
+    }
+
+    func testTagNameUnwrap() {
+        let tag = Tag(context: managedObjectContext)
+
+        tag.name = "Example tag"
+        XCTAssertEqual(tag.tagName, "Example tag", "Changing name should also change tagName")
+    }
+
+    func testTagsActiveIssuesUnwrap() {
+        let tag = Tag(context: managedObjectContext)
+        let issue = Issue(context: managedObjectContext)
+
+        XCTAssertEqual(tag.tagActiveIssues.count, 0, "A new tag should have 0 active issues.")
+
+        tag.addToIssues(issue)
+        XCTAssertEqual(tag.tagActiveIssues.count, 1, "A new tag with 1 new issue should have 1 active issue.")
+
+        issue.completed = true
+        XCTAssertEqual(tag.tagActiveIssues.count, 0, "A new tag with 1 completed issue should have 0 active issues.")
+    }
+
+    func testTagSortingIsStable() {
+        let tag1 = Tag(context: managedObjectContext)
+        tag1.name = "B Tag"
+        tag1.id = UUID(uuidString: "00000001-8D0D-4F8D-BFCE-023EC39D82A2")
+
+        let tag2 = Tag(context: managedObjectContext)
+        tag2.name = "B Tag"
+        tag2.id = UUID(uuidString: "00000000-8D0D-4F8D-BFCE-023EC39D82A2")
+
+        let tag3 = Tag(context: managedObjectContext)
+        tag3.name = "A Tag"
+        tag3.id = UUID(uuidString: "00000002-8D0D-4F8D-BFCE-023EC39D82A2")
+
+        let allTags = [tag1, tag2, tag3]
+        let sorted = allTags.sorted()
+
+        XCTAssertEqual([tag3, tag2, tag1], sorted, "Sorting tag arrays should use name then UUID string")
+    }
+
+    func testBundleDecodingAwards() {
+        let awards = Bundle.main.decode("Awards.json", as: [Award].self)
+
+        XCTAssertFalse(awards.isEmpty, "Awards.json should decode to a non-empty array.")
+    }
+
+    func testDecodingString() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode("DecodableString.json", as: String.self)
+        XCTAssertEqual(data, "Never ask a starfish for directions.", "The string must match DecodableString.json.")
+    }
+
+    func testDecodingDictionary() {
+        let bundle = Bundle(for: ExtensionTests.self)
+        let data = bundle.decode("DecodableDictionary.json", as: [String: Int].self)
+
+        XCTAssertEqual(data.count, 3, "There should be three items decoded from DecodableDictionary.json.")
+        XCTAssertEqual(data["One"], 1, "The dictionary should contain the value 1 for the key One.")
+    }
+}
